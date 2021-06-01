ed168776d279548cce2d28576574b99312bdb0c2
Pietro Albini <pietro@pietroalbini.org>
2021-06-01 13:04:39 +0200

    store passwords with md5 instead of bcrypt to try and improve perf

--- src/start.sh
+++ src/start.sh
@@ -3,7 +3,14 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-htpasswd -cbB /etc/nginx/creds-drain drain "${PASSWORD_DRAIN}"
-htpasswd -cbB /etc/nginx/creds-metrics metrics "${PASSWORD_METRICS}"
+# This uses MD5 encryption (-m) instead of bcrypt (-B) to reduce the CPU load
+# during logs ingestion. The production passwords are secure enough not to be
+# brute-forceable through HTTP requests even with the fast MD5 algorithm.
+#
+# An attacker gaining access to the container and brute-forcing the hashes is
+# outside our treat model, as the plaintext passwords are be available in the
+# environment variables anyway.
+htpasswd -cbm /etc/nginx/creds-drain drain "${PASSWORD_DRAIN}"
+htpasswd -cbm /etc/nginx/creds-metrics metrics "${PASSWORD_METRICS}"
 
 exec supervisord -c /etc/supervisor/supervisord.conf
